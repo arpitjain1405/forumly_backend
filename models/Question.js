e9c06f1321c8695db3a7821a48d955116bfed733
arpitjain1405 <arpitjain1405@gmail.com>
2025-05-30 23:58:42 +0530

    Created question model

---
+++ models/Question.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const questionSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    minLength: 5,
+  },
+  content: {
+    type: String,
+    required: true,
+    minLength: 20,
+  },
+  likes: {
+    type: Number,
+    default: 0,
+    min: 0,
+  },
+  views: {
+    type: Number,
+    default: 0,
+    min: 0,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+const Question = mongoose.Schema('Question', questionSchema);
+
+module.exports = Question;
