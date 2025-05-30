260dc554505eeabc59241d2a3c1d13ee4c1454b2
arpitjain1405 <arpitjain1405@gmail.com>
2025-05-30 23:17:51 +0530

    Created discussion model

---
+++ models/Discussion.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const discussionSchema = new mongoose.Schema({
+  question: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Question",
+  },
+  replies: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Replies",
+    },
+  ],
+  categories: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+    },
+  ],
+  followers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  timestamps: true,
+});
+
+const Discussion = mongoose.model("Discussion", discussionSchema);
+
+module.exports = Discussion;
